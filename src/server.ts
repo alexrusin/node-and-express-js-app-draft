889b12cd4edbcd903395b88f146609740afd7488
Alex Rusin <alexrusin_2000@yahoo.com>
2024-10-21 06:32:36 -0700

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import config from "./config";
+
+export const createServer = () => {
+  const app = express();
+  app
+    .disable("x-powered-by")
+    .use(morgan("dev"))
+    .use(express.urlencoded({ extended: true }))
+    .use(express.json())
+    .use(cors());
+
+  app.get("/health", (req: Request, res: Response) => {
+    res.json({ ok: true, environment: config.env });
+  });
+
+  return app;
+};
